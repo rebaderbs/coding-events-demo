b8f7f5aa3d15ce52fab9b1f0e7c1e0d3f2d3a468
rebaderbs <rderbak@gmail.com>
2022-11-09 19:46:56 -0600

    class 16 studio completed

--- src/main/java/org/launchcode/codingevents/data/EventCategoryRepository.java
+++ src/main/java/org/launchcode/codingevents/data/EventCategoryRepository.java
@@ -1,6 +1,5 @@
 package org.launchcode.codingevents.data;
 
-import org.launchcode.codingevents.models.Event;
 import org.launchcode.codingevents.models.EventCategory;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
