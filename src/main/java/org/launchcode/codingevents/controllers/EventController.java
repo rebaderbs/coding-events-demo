8440cf1b2cae93f4bffdbf7242a6f3a973d64670
rebaderbs <rderbak@gmail.com>
2022-11-22 15:24:40 -0600

    chapter 18 exercises...work in progress

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -33,20 +33,31 @@ public class EventController {
     private TagRepository tagRepository;
 
     @GetMapping
-    public String displayEvents(@RequestParam(required = false) Integer categoryId, Model model) {
+    public String displayEvents(@RequestParam(required = false) Integer categoryId, @RequestParam(required = false) Integer tagId, Model model) {
 
-        if (categoryId == null) {
+        if (categoryId == null & tagId == null) {
             model.addAttribute("title", "All Events");
             model.addAttribute("events", eventRepository.findAll());
         } else {
             Optional<EventCategory> result = eventCategoryRepository.findById(categoryId);
+            Optional<Tag> tagOptional = tagRepository.findById(tagId);
+
             if (result.isEmpty()) {
                 model.addAttribute("title", "Invalid Category ID: " + categoryId);
-            } else {
+            }
+            if (tagOptional.isEmpty())  {
+                model.addAttribute("title", "Invalid Tag ID: " + tagId);
+            }
+            else {
                 EventCategory category = result.get();
                 model.addAttribute("title", "Events in category: " + category.getName());
                 model.addAttribute("events", category.getEvents());
+
+                EventCategory tag = result.get();
+                model.addAttribute("title", "Events in tag: " + tag.getName());
+                model.addAttribute("events", tag.getEvents());
             }
+
         }
 
         return "events/index";
